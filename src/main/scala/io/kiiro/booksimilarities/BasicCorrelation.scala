2ee25704b4f6abe1193d83823c5f7e3c7c5d91b1
Greg Bakos <greg@kiiro.io>
2014-02-28 17:22:19 +0000

    added Cosine similarity

--- src/main/scala/io/kiiro/booksimilarities/BasicCorrelation.scala
+++ src/main/scala/io/kiiro/booksimilarities/BasicCorrelation.scala
@@ -7,24 +7,40 @@ package io.kiiro.booksimilarities
  */
 abstract class BasicCorrelation {
 
-  /** Correlation measurement
+  /** 
+   *  Correlation measurement
    *  
    *  More about Pearson product-moment correlation here:
    *  http://en.wikipedia.org/wiki/Pearson_product-moment_correlation_coefficient
    * 
    * @param size
-   * @param item
+   * @param items
    * @param ratingFirstSum
    * @param ratingSecondSum
    * @param ratingFirstNormSq
    * @param ratingSecondNormSq
    * @return correlation value in Double
    */
-  def pearsonCorrelation(size: Double, item: Double, ratingFirstSum: Double, ratingSecondSum: Double, ratingFirstNormSq: Double,
+  def pearsonCorrelation(size: Double, items: Double, ratingFirstSum: Double, ratingSecondSum: Double, ratingFirstNormSq: Double,
     ratingSecondNormSq: Double) = {
-	  ( size * item - ratingFirstSum * ratingSecondSum ) /
+	  ( size * items - ratingFirstSum * ratingSecondSum ) /
 	  ( scala.math.sqrt( size * ratingFirstNormSq - ratingFirstSum * ratingFirstSum ) *
 	    scala.math.sqrt( size * ratingSecondNormSq - ratingSecondSum * ratingSecondSum ) )
+  }
+  
+  /**
+   * Cosine similarity
+   * 
+   * More about Cosine Similarity here:
+   * http://en.wikipedia.org/wiki/Cosine_similarity
+   * 
+   * @param item
+   * @param ratingFirstNorm
+   * @param ratingSecondNorm
+   * @return similarity value in Double
+   */
+  def cosineSimilarity(items : Double, ratingFirstNorm : Double, ratingSecondNorm : Double) = {
+    items / (ratingFirstNorm * ratingSecondNorm)
   }
 
 }
--- src/test/scala/io/kiiro/booksimilarities/BasicCorrelationTest.scala
+++ src/test/scala/io/kiiro/booksimilarities/BasicCorrelationTest.scala
@@ -5,44 +5,57 @@ import java.text.DecimalFormat
 
 /**
  * Testing basic statistics
- * 
+ *
  * @author Greg Bakos
  */
 class BasicCorrelationTest extends FlatSpec with Matchers {
 
-  /** Correlation measurement Test
-   *  
-   *  online calculator to validate the result:
-   *  http://www.socscistatistics.com/tests/pearson/Default2.aspx
-   *  
-   */
-  it should "equal to " in {
-    
-    // need two Int arrays
-    val sim1 = Array(5,5,4,3,4,5,4,3,3,2,3,2,4,3,2,2,3,4,4,3,2,1,1,3,4,5,4,4,3,2,1,2,3,4,3,2,1,1,2,3,1)
-    val sim2 = Array(5,4,4,3,4,5,4,3,3,2,3,2,4,3,2,2,3,4,4,2,2,1,1,3,4,5,4,4,3,2,1,2,3,4,3,2,5,5,2,3,1)
-    
-    // monitoring value from socscistatistics
-    val r = "0.71"
-    
-    // compute basics
-    val size = sim1.length
-    val item = (sim1,sim2).zipped.map{ case (a,b) => a*b }.reduceLeft(_+_)
-    val ratingFirstSum = sim1.reduceLeft(_+_)
-    val ratingSecondSum = sim2.reduceLeft(_+_)
-    val ratingFirstNormSq = sim1.map{ case a => scala.math.pow(a, 2) }.reduceLeft(_+_)
-    val ratingSecondNormSq = sim2.map{ case a => scala.math.pow(a, 2) }.reduceLeft(_+_)
-    
-    // create a dummy class from abstract class
+  // need two Int arrays
+  val sim1 = Array(5, 5, 4, 3, 4, 5, 4, 3, 3, 2, 3, 2, 4, 3, 2, 2, 3, 4, 4, 3, 2, 1, 1, 3, 4, 5, 4, 4, 3, 2, 1, 2, 3, 4, 3, 2, 1, 1, 2, 3, 1)
+  val sim2 = Array(5, 4, 4, 3, 4, 5, 4, 3, 3, 2, 3, 2, 4, 3, 2, 2, 3, 4, 4, 2, 2, 1, 1, 3, 4, 5, 4, 4, 3, 2, 1, 2, 3, 4, 3, 2, 5, 5, 2, 3, 1)
+
+  // compute basics
+  val items = (sim1, sim2).zipped.map { case (a, b) => a * b }.reduceLeft(_ + _)
+  val ratingFirstSum = sim1.reduceLeft(_ + _)
+  val ratingSecondSum = sim2.reduceLeft(_ + _)
+  val ratingFirstNormSq = sim1.map { case a => scala.math.pow(a, 2) }.reduceLeft(_ + _)
+  val ratingSecondNormSq = sim2.map { case a => scala.math.pow(a, 2) }.reduceLeft(_ + _)
+
+  // create a dummy class from abstract class
     class TestCorrelation extends BasicCorrelation
     val tc = new TestCorrelation
-    
-    // it needs because socscistatistics yields a two decimal point precision result
-    val formatter = new DecimalFormat("#.##")
-    
-    assert(r ===  formatter.format(tc.pearsonCorrelation(size, item, ratingFirstSum, ratingSecondSum, 
-        ratingFirstNormSq, ratingSecondNormSq)) )
-	  
+  
+  /**
+   * Correlation measurement Test
+   *
+   *  online calculator to validate the result:
+   *  http://www.socscistatistics.com/tests/pearson/Default2.aspx
+   *
+   */
+  "0.71 correlation value" should "equal to " in {
+
+    // monitoring value from socscistatistics
+    val r = 0.71
+
+    assert(r === BigDecimal(tc.pearsonCorrelation(sim1.length, items, ratingFirstSum, ratingSecondSum,
+      ratingFirstNormSq, ratingSecondNormSq)).setScale(2, BigDecimal.RoundingMode.HALF_UP).toDouble)
+  }
+  
+  /**
+   * Cosine similarity test
+   *
+   *  online calculator to validate the result:
+   *  http://www.appliedsoftwaredesign.com/archives/cosine-similarity-calculator/
+   *
+   */
+  "0.960949230409 similarity" should "equal to " in {
+
+    // monitoring value from appliedsoftwaredesign
+    val similarity = 0.960949230409
+
+    assert(similarity === 
+      BigDecimal(tc.cosineSimilarity(items, 
+        scala.math.sqrt(ratingFirstNormSq), scala.math.sqrt(ratingSecondNormSq))).setScale(12, BigDecimal.RoundingMode.HALF_UP).toDouble)
   }
 
 }
